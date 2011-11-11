5c8804f371db989198c434aaff4805394508a59d
George Carpenter <gcarpenterv@gmail.com>
2011-11-10 20:26:09 -0500

    asdf

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,9 +4,7 @@ class CatsController < ApplicationController
   # GET /cats/name.json
   def show
     @name = params[:name]
-    if @cats = Cat.find(:all, :conditions => ['name = "'+params[:name]+'"'])
-    else @cats = null
-    end
+    @cats = Cat.where(["\"name\" = '"+params[:name]+"'"])
 
     respond_to do |format|
       format.html # show.html.erb
