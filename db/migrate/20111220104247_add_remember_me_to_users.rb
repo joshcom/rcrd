c8331544a069581d3b3c8928e3a9be9da7fedd8c
Jeff Carpenter <gcarpenterv@gmail.com>
2011-12-20 23:59:10 -0800

    asdf

--- db/migrate/20111220104247_add_remember_me_to_users.rb
+++ db/migrate/20111220104247_add_remember_me_to_users.rb
@@ -1,9 +1,9 @@
 class AddRememberMeToUsers < ActiveRecord::Migration
   def up
-    add_column :users, :remember_me_token, :boolean, :default => 0
+    add_column :users, :remember_me_token, :boolean, :default => false
   end
 
   def down
-    remove_column :users, :remember_me_token, :boolean, :default => 0
+    remove_column :users, :remember_me_token, :boolean, :default => false
   end
 end
