fe8c12fbdd13f6250c03a77277cc3e52605d2ab7
Jeff Carpenter <gcarpenterv@gmail.com>
2011-12-20 23:47:39 -0800

    asdf

--- db/migrate/20111031003807_create_measures.rb
+++ db/migrate/20111031003807_create_measures.rb
@@ -1,10 +1,10 @@
 class CreateMeasures < ActiveRecord::Migration
   def change
-    create_table :measures do |t|
-      t.text :name
-      t.integer :value
-
-      t.timestamps
-    end
+#     create_table :measures do |t|
+#       t.text :name
+#       t.integer :value
+# 
+#       t.timestamps
+#     end
   end
 end
--- db/migrate/20111108190002_change_record_name_type.rb
+++ db/migrate/20111108190002_change_record_name_type.rb
@@ -1,5 +1,5 @@
 class ChangeRecordNameType < ActiveRecord::Migration
   def change
-    add_column :records, :raw, :text
+#     add_column :records, :raw, :text
   end
 end
