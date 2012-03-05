6fd10921c75bc2f6420be3abe22cf4fd6668e824
Jeff Carpenter <hello@jeffcarp.com>
2012-03-05 10:31:47 -0500

    removed movement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,6 +33,21 @@ module ApplicationHelper
   
   def csv_export_helper
     CSV.generate do |csv|
+      "2012-01-01".to_date.upto("2012-01-31".to_date) do |day|        
+        if records = Record.find(:all, :conditions => [" YEAR(created_at) = ? AND MONTH(created_at) = ? AND DAY(created_at) = ?", day.strftime("%Y"), day.strftime("%m"), day.strftime("%d")])
+          records.each do |r|
+            r.cats.each do |c|
+              csv << [@cat_name, day, c.magnitude]
+            end
+          end          
+        end
+        csv << [@cat_name, day, 0]
+      end
+    end
+  end 
+  
+  def old_csv_export_helper
+    CSV.generate do |csv|
       previous_day = '0'
       new_magnitude = 0
       @records.each do |r|          
