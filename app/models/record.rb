7c7acdd9fce48813f2d71ad78f9b1f1d9646ce88
Jeff Carpenter <gcarpenterv@gmail.com>
2013-05-25 14:47:05 -0700

    Fix some small input issues

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,15 +4,18 @@ class Record < ActiveRecord::Base
   accepts_nested_attributes_for :cats, :allow_destroy => true
 
   def self.get_cat_count_per_day(num_days, cat)
-    query = "SELECT date_trunc('day', created_at) AS day, count(*) AS record_count FROM records WHERE created_at > now() - interval '#{num_days} days' AND user_id = 2 AND raw LIKE '%#{cat}%' GROUP BY 1 ORDER BY 1"
+
+    num_days -= 1
+
+    query = "SELECT date_trunc('day', (created_at at time zone 'PDT')) AS day, count(*) AS record_count FROM records WHERE created_at > ((now() - interval '#{num_days} days') at time zone 'PDT') AND user_id = 2 AND raw LIKE '%#{cat}%' GROUP BY 1 ORDER BY 1"
     result = ActiveRecord::Base.connection.execute(query)
 #
     days = {} 
 
-    (Date.today-num_days...Date.today).each do |day|
+    (Date.today-num_days..Date.today).each do |day|
       days[day.strftime('%F')] = 0 
     end
-    
+
     result.each do |res|
       date = Date.parse(res['day']).strftime('%F')
       days[date] = res['record_count']
