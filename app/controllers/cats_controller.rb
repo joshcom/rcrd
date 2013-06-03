d25adb63f8b59cf6dbda8637e4f51bf86a9bf4ff
Jeff Carpenter <gcarpenterv@gmail.com>
2013-06-03 15:32:41 +0900

    A little bit of view refactoring

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,7 +3,8 @@ class CatsController < ApplicationController
   def show
     @name = params[:id]
     @records = Record.get_cats(@name)
-    @trending_cats = Record.get_trending_cats[0..6]
+    @trending_cats = Record.get_trending_cats[0..7]
+    @trending_cats.delete @name
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,12 @@ module ApplicationHelper
   # ===============
 
   def style_info(hue)
-    "background-color: hsl(#{hue}, 80%, 60%);"
+    str = "background-color: hsl(#{hue}, 65%, 48%);"
+    str += "border-bottom: solid 2px hsl(#{hue}, 80%, 20%);"
+    str += "text-shadow: 0 -1px 1px hsl(#{hue}, 30%, 0%);"
   end
 
-  def no_mag(str)
-    # TODO: this should un-pluralize as well
+  def no_mag(str) # Possibly rename to 'tokenize'
     str.gsub! /^\s*\d+\.*\d*\s*/, ''
     str.singularize
   end
