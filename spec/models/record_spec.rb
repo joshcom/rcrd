a00743e2fef10611f4207f29def04105bb61ff98
Jeff Carpenter <gcarpenterv@gmail.com>
2013-06-07 15:53:13 +0900

    Got tests back up to working spec after requiring :user_id on Record model

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -3,25 +3,29 @@ require "spec_helper"
 describe Record do
 
   it "get_cats " do
-    one = Record.create!(raw: "workout, swim", target: Time.now)
-    two = Record.create!(raw: "workout, swim", target: Time.now)
+    user = User.create!(email: "whatever@jeff.is")
+    one = user.records.create!(raw: "workout, swim", target: Time.now)
+    two = user.records.create!(raw: "workout, swim", target: Time.now)
 
     expect(Record.get_cats('swim').count).to eq(2)
   end
 
   it "cats_from_raw" do
-    one = Record.create!(raw: "workout, swim, 3200 yards", target: Time.now)
+    user = User.create!(email: "whatever@jeff.is")
+    one = user.records.create!(raw: "workout, swim, 3200 yards", target: Time.now)
     expect(one.cats_from_raw).to eq(['workout', 'swim', '3200 yards'])
   end
 
   # TODO: test for decimals, weird inputs
   it "cats_from_raw_without_mags" do
-    one = Record.create!(raw: "workout, swim, 3200 yards", target: Time.now)
+    user = User.create!(email: "whatever@jeff.is")
+    one = user.records.create!(raw: "workout, swim, 3200 yards", target: Time.now)
     expect(one.cats_from_raw_without_mags).to eq(['workout', 'swim', 'yards'])
   end
 
   it "calculates hue correctly" do
-    one = Record.create!(raw: "workout, swim, 3200 yards", target: Time.now)
+    user = User.create!(email: "whatever@jeff.is")
+    one = user.records.create!(raw: "workout, swim, 3200 yards", target: Time.now)
 
     # Do our own arithmetic to check
     minutes = one.target.strftime('%k').to_f * 60.0
@@ -32,13 +36,15 @@ describe Record do
   end
 
   it "gets the default time_zone_text" do
-    one = Record.create!(raw: "workout, swim, 3200 yards", target: Time.now)
+    user = User.create!(email: "whatever@jeff.is")
+    one = user.records.create!(raw: "workout, swim, 3200 yards", target: Time.now)
     expect(one.time_zone_text).to eq("Pacific Time (US & Canada)")
   end
 
   it "gets the correct time_zone_text" do
-    tz = Record.create!(raw: "time zone, Tokyo", target: Time.now - 5.minutes)
-    one = Record.create!(raw: "workout, swim, 3200 yards", target: Time.now)
+    user = User.create!(email: "whatever@jeff.is")
+    tz = user.records.create!(raw: "time zone, Tokyo", target: Time.now - 5.minutes)
+    one = user.records.create!(raw: "workout, swim, 3200 yards", target: Time.now)
     expect(one.time_zone_text).to eq("Tokyo")
   end
 
--- spec/requests/records_spec.rb
+++ spec/requests/records_spec.rb
@@ -8,7 +8,8 @@ describe "Records" do
   end
 
   it "should not be able to load a record without authenticating" do
-    one = Record.create!(raw: "workout, swim", target: Time.now)
+    user = User.create!(email: "whatever@jeff.is")
+    one = user.records.create!(raw: "workout, swim", target: Time.now)
     get record_path one
     response.status.should be(302) # should redirect to root_url
   end
@@ -21,9 +22,9 @@ describe "Records" do
   end
 
   it "should be able to load a record once authenticated" do
-    one = User.create!(email: "hi@jeff.is")
-    get "/sessions/new?passcode=#{ENV['RCRD_PASSCODE']}&id=#{one.id.to_s}"
-    two = Record.create!(raw: "workout, swim", target: Time.now)
+    user = User.create!(email: "hi@jeff.is")
+    get "/sessions/new?passcode=#{ENV['RCRD_PASSCODE']}&id=#{user.id.to_s}"
+    two = user.records.create!(raw: "workout, swim", target: Time.now)
     get record_path two 
     response.status.should be(200)
   end
