f3de0113531787686f252b044c75a0cbdb77a21b
Jeff Carpenter <gcarpenterv@gmail.com>
2013-06-14 11:15:05 -0700

    Minor style fix, add walk to tracked cats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,19 @@
 class HomeController < ApplicationController
 
+#  def index
+    # aggregate of everyone's cats
+    # log in
+
+#  end
+
   def index 
     @num_days = 60  
     @cats = [
       {name: 'workout', color: '96b4fd', avgs: true}, 
       {name: 'swim', color: '032780', avgs: true}, 
-      {name: 'run', color: '009942', avgs: true}, 
-      {name: 'drink', color: 'D42627', avgs: true}] 
+      {name: 'run', color: '009942', avgs: true, day_avgs: true}, 
+      {name: 'drink', color: 'D42627', avgs: true, mag_avgs: true},
+      {name: 'walk', color: 'cf0b7b'}] 
     @cats.each do |cat|
       cat[:days] = {}
       cat[:days] = Record.get_cat_count_per_day(@num_days, cat[:name])
