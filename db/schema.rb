ee419f5409f6b96bf036274bb63b8186757011e7
Jeff Carpenter <gcarpenterv@gmail.com>
2013-06-15 17:00:10 -0700

    Add new schema.rb after heroku db:pull

--- db/schema.rb
+++ db/schema.rb
@@ -13,22 +13,82 @@
 
 ActiveRecord::Schema.define(:version => 20130615221523) do
 
-  create_table "records", :force => true do |t|
+  create_table "categories", :force => true do |t|
     t.text     "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cats", :force => true do |t|
+    t.integer  "cat_id"
+    t.text     "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "magnitude"
+    t.integer  "karma"
+    t.integer  "record_id"
+  end
+
+  create_table "cats_records", :id => false, :force => true do |t|
+    t.integer  "cat_id"
+    t.integer  "record_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "evil_wizards", :force => true do |t|
+    t.integer  "cat_id"
+    t.integer  "magnitude"
+    t.integer  "karma"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "karmas", :force => true do |t|
+    t.string   "name"
+    t.decimal  "points",     :precision => 8, :scale => 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "records", :force => true do |t|
+    t.integer  "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "raw"
     t.integer  "user_id"
     t.datetime "target"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sorcerers", :force => true do |t|
+    t.integer  "cat_id"
+    t.integer  "magnitude"
+    t.integer  "karma"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
     t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
     t.string   "time_zone"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.integer  "karma"
     t.string   "password_hash"
     t.string   "password_salt"
+  end
+
+  create_table "wizards", :force => true do |t|
+    t.integer  "cat_id"
+    t.integer  "magnitude"
+    t.integer  "karma"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
