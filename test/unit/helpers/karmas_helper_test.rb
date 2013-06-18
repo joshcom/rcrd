fb7b7934346ab25b1500c8bfca5b96c8f91fafe8
Jeff Carpenter <gcarpenterv@gmail.com>
2013-06-18 02:20:25 -0700

    Remove test/ directory

--- test/unit/helpers/karmas_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class KarmasHelperTest < ActionView::TestCase
-end
--- test/unit/karma_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class KarmaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
